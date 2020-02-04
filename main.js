66416fec0d372d839866ffb66313514bc92755d6
Nick Algarra <algarran24@gmail.com>
2020-02-04 15:37:15 -0600

    Finishing touches on responsiveness and links

--- main.js
+++
@@ -1,8 +0,0 @@
-
-
-function clickListener() {
-    document.getElementById('copy_btn').select();
-    document.execCommand('copy');
-}
-
-document.getElementById('copy_btn').addEventListener('click', clickListener);
